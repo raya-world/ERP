1e6862b2da26c4b12249167be463f418fef7f688
krupalvora <krupalvora789@gmail.com>
2025-12-07 09:39:47 +0000

    St dim: naing conv hard code

--- raya_erp/raya_erp/doctype/stone_dimension/stone_dimension.py
+++ raya_erp/raya_erp/doctype/stone_dimension/stone_dimension.py
@@ -2,8 +2,11 @@
 # For license information, please see license.txt
 
 # import frappe
+from annotated_types import doc
 from frappe.model.document import Document
 
 
 class StoneDimension(Document):
-	pass
+	def before_save(self):
+		tmp = f"{self.shape or ''}-{self.cut or ''}-{self.origin or ''}-{self.purity or ''}-{self.gradecolour or ''}-{self.stone_type or ''}-{str(self.length)}x{str(self.width)}x{str(self.height)}"
+		self.name = tmp
