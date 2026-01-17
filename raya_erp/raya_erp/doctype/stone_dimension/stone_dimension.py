212e2b1a52d19ab731312e04a020f3e858537733
krupalvora <krupalvora789@gmail.com>
2026-01-17 15:50:33 +0000

    feat: added new fileds in ring size

--- raya_erp/raya_erp/doctype/stone_dimension/stone_dimension.py
+++ raya_erp/raya_erp/doctype/stone_dimension/stone_dimension.py
@@ -8,5 +8,6 @@ from frappe.model.document import Document
 
 class StoneDimension(Document):
 	def before_save(self):
-		tmp = f"{self.shape or ''}-{self.cut or ''}-{self.origin or ''}-{self.purity or ''}-{self.gradecolour or ''}-{self.stone_type or ''}-{str(self.length)}x{str(self.width)}x{str(self.height)}"
+		# {Stone_name}-{Shape}-{Stone_type}-{Cut_Style}-{Clarity_grade}-{Colour_grade}-{length}x{width}x{height}
+		tmp = f"{self.family or ''}-{self.shape or ''}-{self.stone_type or ''}-{self.cut or ''}-{self.cut_grade or ''}-{self.gradecolour or ''}-{str(self.length)}x{str(self.width)}x{str(self.height)}"
 		self.name = tmp
